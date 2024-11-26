36b303e07b1f5ea4b417382c2a6f8e7e3c2957f5
Flavie <flavie1.fremont@epitech.digital>
2024-11-26 16:50:05 +0100

    feat: Mise en place et stylisation de la page de modification du profil
    - Ajout de champs pour le nom complet, surnoms, email, numéro de téléphone, pays, genre, et adresse.
    - Intégration de ScrollView pour permettre le défilement sur la page.
    - Utilisation de @react-native-picker/picker pour les sélecteurs de pays et genre.
    - Amélioration des styles : ajout d'une légère couleur de fond, arrondis pour les champs, et espacement harmonieux.
    - Stylisation du bouton de soumission avec un design moderne et ombrage.

---
+++ evolut-ia/app/ModifProfil.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import EditProfile from "../components/ModifProfil"; // Importation du composant EditProfile
+
+const ModifProfil: React.FC = () => {
+  return <EditProfile />;
+};
+
+export default ModifProfil;
